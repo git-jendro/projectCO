623a1b8cae080197286df2b97b8aa170029d44d9
git-jendro <jendro.hard@gmail.com>
2020-06-24 18:22:05 +0700

    finishing UI  template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,76 +3,36 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12 mb-3">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                <form action="/home" method="post">
+                    @csrf
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Perasaan anda ?</span>
                         </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+                            <textarea class="form-control" aria-label="With textarea" name="threads" placeholder="Tulisakan keluh kesah anda disini..."></textarea>
+                            <button type="submit" class="btn btn-primary ml-2">Kirim</button>
+                        </div>
+                </form>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
-
-
-{{-- 
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
-            <form action="/home" method="post">
-                @csrf
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text">Perasaan anda ?</span>
-                    </div>
-                        <textarea class="form-control" aria-label="With textarea" name="threads" placeholder="Tulisakan keluh kesah anda disini..."></textarea>
-                        <button type="submit" class="btn btn-primary ml-2">Kirim</button>
-                    </div>
-            </form>
         </div>
     </div>
 </div>
 @endsection
 
 @section('thread')
-
-@foreach ($threads as $thread)
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-          {{$thread -> user_id}}
-        <a href="/students/{{$student->id}}" class="badge badge-info">Detail</a>
-        </li>
-      </ul>
-      @endforeach
-
-      
-    @foreach ($threads as $thread)
-      <div class="card mb-5">
-        <div class="card-header">{{$thread -> id_users}} 
-            <div class="card-date">
-                {{$thread -> created_at}}
-            </div>
+<div class="card mt-3">
+    <div class="card-header">Nama 
+        <div class="card-date">
+            Tanggal
         </div>
-      
-          <div class="card-body">
-              {{$thread -> threads}}
-          </div>
+    </div>
+  
+      <div class="card-body">
+          Threads
       </div>
-      
-    @endforeach
-@endsection --}}
+  </div>
+    
+@endsection
+
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -70,8 +70,8 @@
         </nav>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-2 px-1 bg-white shadow-sm position-fixed" id="sticky-sidebar">
-                    <div class="nav flex-column flex-nowrap vh-100 overflow-auto text-white p-2">
+                <div class="col-2 px-1 bg-white shadow-sm position-fixed pt-2" id="sticky-sidebar">
+                    <div class="nav flex-column flex-nowrap vh-100 overflow-auto text-white p-4 align-content-center">
                         <img src="{{ asset('img/1.png') }}" class="avatar" alt="avatar">
                         <a href="./" class="nav-link">{{ Auth::user()->name }}</a>
                         <a href="./" class="nav-link">Total Update</a>
@@ -91,103 +91,6 @@
                 </div>
             </div>
         </div>
-    </div>
-</body>
-</html>
-{{-- <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="container-fluid">
-                <div class="row">
-                    <div class="col-2 px-1 bg-dark position-fixed" id="sticky-sidebar">
-                        <div class="nav flex-column flex-nowrap vh-100 overflow-auto text-white p-2">
-                            <img src="{{ asset('img/1.png') }}" class="avatar" alt="avatar">
-                            <a href="./" class="nav-link">{{ Auth::user()->name }}</a>
-                            <a href="./" class="nav-link">Total Update</a>
-                        </div>
-                    </div>
-                    <div class="col offset-2" id="main">
-                        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 mt-3 border-bottom">
-                            @yield('content')
-                        </div>
-                        
-                                    @yield('thread')
-                    </div>
-                </div>
-            </div> --}}
-        {{-- <main class="py-4">
-            @yield('content')
-        </main> --}}
     </div>
 </body>
 </html>
